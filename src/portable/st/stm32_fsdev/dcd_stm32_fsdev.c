fedcda8e305d1f46904ad33d9bc43284fcfa1579
RockyZeroFour <markus@wooting.io>
2024-05-01 05:18:39 +0200

    Remove generic USB wakeup interrupt
    - The generic USBD_WKUP_IRQn don't belong to the USB stack and instead should be controlled by the app

--- src/portable/st/stm32_fsdev/dcd_stm32_fsdev.c
+++ src/portable/st/stm32_fsdev/dcd_stm32_fsdev.c
@@ -323,10 +323,10 @@ void dcd_int_enable (uint8_t rhport)
   NVIC_EnableIRQ(USB_HP_CAN1_TX_IRQn);
   NVIC_EnableIRQ(USB_LP_CAN1_RX0_IRQn);
   NVIC_EnableIRQ(USBWakeUp_IRQn);
+
 #elif CFG_TUSB_MCU == OPT_MCU_GD32F303
   NVIC_EnableIRQ(USBD_HP_CAN0_TX_IRQn);
   NVIC_EnableIRQ(USBD_LP_CAN0_RX0_IRQn);
-  NVIC_EnableIRQ(USBD_WKUP_IRQn);
 
 #elif CFG_TUSB_MCU == OPT_MCU_STM32G4
   NVIC_EnableIRQ(USB_HP_IRQn);
